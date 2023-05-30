8fd1a742778e80be2aa1c99936a2f8d932171f07
hsmgowtham <hsmgowtham@gmail.com>
2023-05-30 23:00:09 +0530

    fixed entity not found bug

--- forward_messages.py
+++ forward_messages.py
@@ -1,10 +1,10 @@
 from os import environ
 from dotenv import load_dotenv
 from datetime import datetime, timedelta, timezone
-
-load_dotenv(".env")
+from telethon.tl.types import PeerChannel
 from telethon.sync import TelegramClient
 
+load_dotenv(".env")
 
 # Get Config from Env File
 API_ID = environ.get("API_ID")
@@ -14,11 +14,7 @@ MESSAGE_SINCE_HR = int(environ.get("MESSAGE_SINCE_HR"))
 
 CHANNEL_ID = None
 CONTACT_CHAT_IDS = []
-for key, val in environ.items():
-    if key.startswith("CONTACT_ID"):
-        CONTACT_CHAT_IDS.append(int(val))
-    elif key.startswith("CHANNEL_ID"):
-        CHANNEL_ID = int(val)
+
 
 # Create a Telegram client
 client = TelegramClient("session", API_ID, API_HASH)
@@ -27,18 +23,36 @@ client = TelegramClient("session", API_ID, API_HASH)
 client.start(phone=PHONE_NUMBER)
 
 async def forward_messages():
-    now = datetime.now(timezone.utc)
-    last_hour = now - timedelta(hours=MESSAGE_SINCE_HR)
-    
-    messages = await client.get_messages(CHANNEL_ID, limit=100)
-    if messages:
-        for message in messages:
-            print(f"{message.date} - {message}")
-            if message.date > last_hour:
-                for contact_chat_id in CONTACT_CHAT_IDS:
-                    await client.forward_messages(contact_chat_id, message, from_peer=CHANNEL_ID)
-    else:
-        print("No messages found in the last hour.")
+    async with TelegramClient("session_name", API_ID, API_HASH) as client:
+        CONTACT_CHAT_IDS = []
+        CHANNEL_ID = None
+
+        for key, val in environ.items():
+            if key.startswith("CONTACT_ID"):
+                CONTACT_CHAT_IDS.append(int(val))
+            elif key.startswith("CHANNEL_ID"):
+                CHANNEL_ID = await client.get_entity(PeerChannel(int(val)))
+                print(CHANNEL_ID)
+
+        now = datetime.now(timezone.utc)
+        specified_hours = now - timedelta(hours=MESSAGE_SINCE_HR)
+
+        message_forwarded_count = 0
+        messages = await client.get_messages(CHANNEL_ID, limit=100)
+        print("-" * 50)
+        if messages:
+            for message in messages:
+                if message.date > specified_hours:
+                    message_forwarded_count += 1
+                    for contact_chat_id in CONTACT_CHAT_IDS:
+                        await client.forward_messages(
+                            contact_chat_id, message, from_peer=CHANNEL_ID
+                        )
+            with open("./logs.txt", "a") as file:
+                file.write(f"\n{datetime.now().strftime('%d/%m/%Y %H-%M-%S')} - Total Messages Forwarded in the last {MESSAGE_SINCE_HR} hr's = {message_forwarded_count}")
+        else:
+            print("No messages found in the last hour.")
+
 
 # Forward messages shared in the last hour to all contacts
 with client:
